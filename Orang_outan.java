ee29c714e8531d834f925cafe637e359bc2e76e4
rom1313 <76427184+rom1313@users.noreply.github.com>
2021-04-13 16:47:44 +0200

    Create Orang_outan.java

---
+++ Orang_outan.java
@@ -0,0 +1,9 @@
+public class Orang_outan extends Animal{
+
+
+    public Orang_outan(String race, String nom) {
+        super(race, nom);
+        this.setRegimeAlimentaire("Végétarien");
+        this.setRepasKilos(5);
+    }
+}
