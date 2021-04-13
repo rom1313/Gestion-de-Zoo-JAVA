b44296df31825fdf0e6b7f6ce948baa85d78bef5
rom1313 <76427184+rom1313@users.noreply.github.com>
2021-04-13 16:46:59 +0200

    Create Chimpanze.java

---
+++ Chimpanze.java
@@ -0,0 +1,8 @@
+public class Chimpanze extends Animal{
+
+    public Chimpanze(String race, String nom) {
+        super(race, nom);
+        this.setRegimeAlimentaire("Végétarien");
+        this.setRepasKilos(3);
+    }
+}
