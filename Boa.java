970472005c900960c1f2ed12835de68c2f16ff30
rom1313 <76427184+rom1313@users.noreply.github.com>
2021-04-13 16:48:54 +0200

    Create Boa.java

---
+++ Boa.java
@@ -0,0 +1,8 @@
+public class Boa extends Animal{
+
+    public Boa(String race, String nom) {
+        super(race, nom);
+        this.setRegimeAlimentaire("Carnivore");
+        this.setRepasKilos(1);
+    }
+}
